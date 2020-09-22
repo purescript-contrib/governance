bb2f9237aace1c06dd98b4f59a079894dd1a075d
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-09-21 19:29:50 -0700

    Fetch all non-draft releases and fix any potential markdown headers
    - GH only provides 30 releases before you must specify more via the 
    `per_page` query (max 100). If there are more than 100, you must get the 
    next page.
    - if the release notes include markdown headers, they will use the same 
    header levels as the release itself. To fix that, we increase the header 
    level by adding a hashtag in front of it.
    - GH includes "draft" releases if you have push-access to a repo. To 
    remove those, we also filter out them via the "draft" field

--- updater/src/Updater/Generate/Changelog.purs
+++ updater/src/Updater/Generate/Changelog.purs
@@ -4,14 +4,18 @@ import Prelude
 
 import Affjax as AX
 import Affjax.ResponseFormat as RF
+import Data.Array (filter, null)
 import Data.Codec (decode)
 import Data.Codec.Argonaut (JsonCodec, array, printJsonDecodeError)
 import Data.Codec.Argonaut as CA
 import Data.Codec.Argonaut.Record as CAR
 import Data.Either (Either(..))
 import Data.Foldable (foldl)
+import Data.Maybe (Maybe(..))
+import Data.Monoid (power)
 import Data.String.CodeUnits (takeWhile)
 import Data.String.Common (joinWith, trim)
+import Data.String.Utils (lines, startsWith)
 import Effect.Aff (Aff, error, throwError)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff as FSA
@@ -21,6 +25,7 @@ type ReleaseInfo =
   , html_url :: String
   , body :: String
   , published_at :: String
+  , draft :: Boolean
   }
 
 releaseCodec :: JsonCodec ReleaseInfo
@@ -30,13 +35,60 @@ releaseCodec =
     , html_url: CA.string
     , body: CA.string
     , published_at: CA.string
+    , draft: CA.boolean
     }
 
 appendReleaseInfoToChangelog :: forall r. { owner :: String, repo :: String | r } -> Aff Unit
 appendReleaseInfoToChangelog gh = do
+  releases <- recursivelyFetchReleases [] 1 gh
+  let
+    realReleases = filter (\r -> r.draft == false) releases
+    appendContent = foldl addReleaseInfo "" realReleases
+  FSA.appendTextFile UTF8 "./CHANGELOG.md" appendContent
+  where
+    addReleaseInfo :: String -> ReleaseInfo -> String
+    addReleaseInfo acc rec =
+      let
+        dateWithoutTimeZone = takeWhile (_ /= 'T') rec.published_at
+        bodyWithFixedHeaders = fixHeaders $ trim rec.body
+      in acc <> joinWith "\n"
+        [ "## [" <> rec.tag_name <> "](" <> rec.html_url <> ") - " <> dateWithoutTimeZone
+        , ""
+        , bodyWithFixedHeaders
+        , ""
+        , ""
+        ]
+
+    fixHeaders :: String -> String
+    fixHeaders s =
+      let
+        incAllHeaderLevels =
+          incHeaderlevel 5
+            >>> incHeaderlevel 4
+            >>> incHeaderlevel 3
+            >>> incHeaderlevel 2
+            >>> incHeaderlevel 1
+      in
+        joinWith "\n" $ map incAllHeaderLevels (lines s)
+
+    incHeaderlevel :: Int -> String -> String
+    incHeaderlevel level line =
+      if startsWith ((power "#" level) <> " ") line
+        then "#" <> line
+        else line
+
+recursivelyFetchReleases :: forall r. Array ReleaseInfo -> Int -> { owner :: String, repo :: String | r } -> Aff (Array ReleaseInfo)
+recursivelyFetchReleases accumulator page gh = do
+  pageNResult <- fetchNextPageOfReleases page gh
+  case pageNResult of
+    Nothing -> pure accumulator
+    Just arr -> recursivelyFetchReleases (accumulator <> arr) (page + 1) gh
+
+fetchNextPageOfReleases :: forall r. Int -> { owner :: String, repo :: String | r } -> Aff (Maybe (Array ReleaseInfo))
+fetchNextPageOfReleases page gh = do
   -- For example
   -- https://api.github.com/repos/purescript-contrib/purescript-http-methods/releases
-  let url = "https://api.github.com/repos/" <> gh.owner <> "/" <> gh.repo <> "/releases"
+  let url = "https://api.github.com/repos/" <> gh.owner <> "/" <> gh.repo <> "/releases?per_page=100&page=" <> show page
 
   result <- AX.get RF.json url
   case result of
@@ -46,18 +98,7 @@ appendReleaseInfoToChangelog gh = do
       case decode (array releaseCodec) body of
         Left e -> do
           throwError $ error $ printJsonDecodeError e
+        Right releases | null releases ->
+          pure Nothing
         Right releases -> do
-          let appendContent = foldl addReleaseInfo "" releases
-          FSA.appendTextFile UTF8 "./CHANGELOG.md" appendContent
-
-  where
-    addReleaseInfo :: String -> ReleaseInfo -> String
-    addReleaseInfo acc rec =
-      let dateWithoutTimeZone = takeWhile (_ /= 'T') rec.published_at
-      in acc <> joinWith "\n"
-        [ "## [" <> rec.tag_name <> "](" <> rec.html_url <> ") - " <> dateWithoutTimeZone
-        , ""
-        , trim rec.body
-        , ""
-        , ""
-        ]
+          pure $ Just releases
