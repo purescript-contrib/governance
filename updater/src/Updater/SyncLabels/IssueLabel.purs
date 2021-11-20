6dabe20f4582c88f92ad4e19661014729cd64178
JordanMartinez <jordanalex.martinez@gmail.com>
2021-11-20 05:11:48 -0800

    `list-labels` command: render color; print repo number (#48)
    * Render color via plugin; print # of repos
    
    * Add '#' to visualize colors in vs code

--- updater/src/Updater/SyncLabels/IssueLabel.purs
+++ updater/src/Updater/SyncLabels/IssueLabel.purs
@@ -1,11 +1,14 @@
 module Updater.SyncLabels.IssueLabel where
 
+import Prelude
+
 import Data.Argonaut.Core (Json, fromString, isString)
 import Data.Codec ((<~<))
 import Data.Codec.Argonaut as CA
 import Data.Codec.Argonaut.Migration as CAM
 import Data.Codec.Argonaut.Record as CAR
 import Data.Maybe (Maybe(..))
+import Data.String (drop)
 
 -- | A GitHub issue label consisting of three parts:
 -- |
@@ -33,33 +36,33 @@ issueLabelCodec =
 
 -- | The full set of issue labels supported by Contributor libraries.
 labels :: Array IssueLabel
-labels =
+labels = map (\r -> r { color = drop 1 r.color })
   [ { name: "breaking change"
     , description: "A change that requires a major version bump"
-    , color: "e99695"
+    , color: "#e99695"
     }
   , { name: "bug"
     , description: "A legitimate bug in the library that ought to be fixed"
-    , color: "d73a4a"
+    , color: "#d73a4a"
     }
   , { name: "document me"
     , description: "Improvements or additions to documentation"
-    , color: "0075ca"
+    , color: "#0075ca"
     }
   , { name: "enhancement"
     , description: "An addition to the library"
-    , color: "a6e1ea"
+    , color: "#a6e1ea"
     }
   , { name: "good first issue"
     , description: "Good for newcomers"
-    , color: "7057ff"
+    , color: "#7057ff"
     }
   , { name: "help wanted"
     , description: "Maintainers would like assistance with solving this issue"
-    , color: "006b75"
+    , color: "#006b75"
     }
   , { name: "question"
     , description: "Question that needs an answer"
-    , color: "fbca04"
+    , color: "#fbca04"
     }
   ]
